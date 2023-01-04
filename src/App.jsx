8dbd53b17778131fefb5bff754eeacc58ee1128e
Conrado Alexsander Soares Gonçalves <53683786+conradoalexsander@users.noreply.github.com>
2023-01-04 10:50:06 +0100

    Added initial project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Conrado Alexsander"
+        content="Amet commodo exercitation cupidatat ullamco."
+      />
+      <Post
+        author="John Doe"
+        content="Amet commodo exercitation cupidatat ullamco."
+      />
+    </div>
+  )
+}
