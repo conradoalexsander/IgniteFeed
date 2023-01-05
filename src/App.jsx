024e5366257b290e8469bdda28e1630bb343b277
Conrado Alexsander Soares Gonçalves <53683786+conradoalexsander@users.noreply.github.com>
2023-01-05 12:10:06 +0100

    Added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
+import "./global.css"
 import { Post } from './Post'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Conrado Alexsander"
         content="Amet commodo exercitation cupidatat ullamco."
